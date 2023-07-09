82f4d016f431488c5df31e5d4c09a60b240334e8
xennia316 <xennia316@gmail.com>
2023-07-09 18:43:20 +0100

    basic setup

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import Header from "./components/header/Header.component";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Header />
+			<Text>This is a text</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
---
+++ client/components/header/Header.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const Header = () => {
+	return (
+		<View style={styles.headerContainer}>
+			<Text>Header</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	headerContainer: {},
+});
+
+export default Header;
