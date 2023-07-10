c722f7676896b2de9dc7fd8eeb0d227271d7c10e
xennia316 <xennia316@gmail.com>
2023-07-10 21:25:45 +0100

    add: new comp

---
+++ client/components/detailTop/detailTop.component.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Text, TouchableOpacity, View, Image } from "react-native";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import pic from "../../assets/images/Tomato.png";
+import fullstar from "../../assets/images/fullStar.png";
+
+import styles from "./detailTop";
+
+const DetailTop = () => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.top}>
+				<TouchableOpacity>
+					<Ionicons
+						name="ios-arrow-back-circle-outline"
+						color="lightgray"
+						size={40}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<Ionicons name="ios-heart-circle" color="lightgray" size={40} />
+				</TouchableOpacity>
+			</View>
+			<View>
+				<Text style={styles.heading}>Italian Hot Pizza</Text>
+				<View style={styles.midContainer}>
+					<View style={styles.mid}>
+						<Image source={pic} style={styles.img} />
+						<Text>Pizza Italiano</Text>
+					</View>
+					<View style={styles.mid}>
+						<Image source={fullstar} style={styles.img} />
+						<Text>
+							4.8<Text style={{ color: "grey" }}>(2.2k review)</Text>
+						</Text>
+						<Ionicons name="ios-chevron-forward" size={16} />
+					</View>
+				</View>
+			</View>
+			<Image style={styles.largeImg} source={pic} />
+			<View style={styles.midContainer}>
+				<TouchableOpacity>
+					<Text style={{ fontWeight: "bold" }}>Details</Text>
+				</TouchableOpacity>
+				<View style={styles.mid}>
+					<Ionicons name="ios-time-outline" size={20} />
+					<Text style={{ fontWeight: "300" }}>15 mins</Text>
+				</View>
+			</View>
+			<Text style={styles.text}>
+				Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+				Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+				Lorem ipsum Lorem ipsum Lorem ipsum
+			</Text>
+		</View>
+	);
+};
+
+export default DetailTop;
---
+++ client/components/detailTop/detailTop.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+	container: { width: "100%", flexDirection: "column", gap: 18 },
+	top: {
+		flexDirection: "row",
+		width: "100%",
+		// height: "100%",
+		justifyContent: "space-between",
+	},
+
+	heading: {
+		fontSize: 24,
+		fontWeight: "400",
+		fontFamily: "Poppins",
+	},
+
+	mid: {
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 4,
+	},
+
+	midContainer: {
+		flexDirection: "row",
+		width: "100%",
+		justifyContent: "space-between",
+		paddingVertical: 12,
+	},
+
+	img: { width: 20, height: 20 },
+
+	largeImg: {
+		width: "100%",
+		height: 300,
+		backgroundColor: "black",
+		borderRadius: 32,
+	},
+	text: {
+		color: "grey",
+		fontSize: 14,
+		marginTop: -10,
+	},
+});
