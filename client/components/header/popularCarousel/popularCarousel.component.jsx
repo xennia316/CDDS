d5dda35ff2809152765f56f15a01a703a31d7ae4
xennia316 <xennia316@gmail.com>
2023-07-10 17:34:41 +0100

    add: carousel item

---
+++ client/components/header/popularCarousel/popularCarousel.component.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import {
+	View,
+	Text,
+	FlatList,
+	ImageBackground,
+	Image,
+	TouchableOpacity,
+	TouchableHighlight,
+} from "react-native";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { PopularCard } from "../index.js";
+import data from "../static.js";
+
+import styles from "./popularCarousel.js";
+import fullstar from "../../assets/images/fullStar.png";
+
+const PopularCarousel = () => {
+	const [selectedItem, setSelectedItem] = useState(null);
+	const renderItems = ({ item }) => {
+		const isSelected = selectedItem === item.id;
+		const backgroundColor = isSelected ? "#F7F7F7" : "#262628";
+		const marginTop = isSelected ? -10 : 0;
+		return (
+			<TouchableHighlight onPress={() => setSelectedItem(item.id)}>
+				<ImageBackground
+					source={item.image}
+					style={[styles.card, { backgroundColor, marginTop }]}
+				>
+					<View style={styles.inner}>
+						<Text style={styles.textName}>{item.name}</Text>
+						<View style={styles.middle}>
+							<Text style={styles.textPrice}>{item.price}</Text>
+							<View style={styles.imageContainer}>
+								<Image style={styles.img} source={fullstar} />
+								<Text style={styles.textRating}>{item.rating}</Text>
+							</View>
+						</View>
+						<View style={styles.bottom}>
+							<TouchableOpacity style={styles.bigButton}>
+								<Text style={styles.addText}>Add to Cart</Text>
+							</TouchableOpacity>
+							<TouchableOpacity style={styles.message}>
+								<View style={styles.blurView} />
+								<Ionicons
+									size={18}
+									style={styles.icon}
+									name="ios-mail-open-outline"
+								/>
+							</TouchableOpacity>
+							<TouchableOpacity style={styles.like}>
+								<View style={styles.blurView} />
+								<Ionicons
+									style={styles.icon}
+									size={18}
+									name="ios-heart-sharp"
+								/>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</ImageBackground>
+			</TouchableHighlight>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.heading}>Popular items</Text>
+			<View>
+				<FlatList
+					style={styles.carousel}
+					horizontal
+					data={data}
+					renderItem={renderItems}
+					showsHorizontalScrollIndicator={false}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default PopularCarousel;
