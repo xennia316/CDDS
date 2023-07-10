667da27f989694a1ea484d294fdd466f5e4e9cd8
xennia316 <xennia316@gmail.com>
2023-07-10 21:23:44 +0100

    static data

---
+++ client/components/static.js
@@ -0,0 +1,38 @@
+import pic from "../assets/images/mexicangreenwave.png";
+export default data = [
+	{
+		id: 1,
+		image: pic,
+		name: "Pepperoni pizza",
+		price: "$5.69",
+		rating: "4.0 (3.8k)",
+	},
+	{
+		id: 2,
+		image: pic,
+		name: "Pepperoni pizza",
+		price: "$5.69",
+		rating: "4.0 (3.8k)",
+	},
+	{
+		id: 3,
+		image: pic,
+		name: "Pepperoni pizza",
+		price: "$5.69",
+		rating: "4.0 (3.8k)",
+	},
+	{
+		id: 4,
+		image: pic,
+		name: "Pepperoni pizza",
+		price: "$5.69",
+		rating: "4.0 (3.8k)",
+	},
+	{
+		id: 5,
+		image: pic,
+		name: "Pepperoni pizza",
+		price: "$5.69",
+		rating: "4.0 (3.8k)",
+	},
+];
