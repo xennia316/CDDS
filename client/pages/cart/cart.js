81b3f2fb54b71e84140077cc5764227ff46d6e12
xennia316 <xennia316@gmail.com>
2024-05-31 04:53:35 +0100

    fix width and padding issues

--- client/pages/cart/cart.js
+++ client/pages/cart/cart.js
@@ -2,25 +2,26 @@ import { StyleSheet } from "react-native";
 export default styles = StyleSheet.create({
 	top: {
 		width: "100%",
-		alignItems: "center",
+		paddingVertical: 0,
+		marginVertical: 0,
 	},
 	container: {
 		height: "100%",
 		width: "100%",
 		backgroundColor: "#fff",
 		alignItems: "center",
-		paddingHorizontal: 12,
+		paddingHorizontal: 18,
 	},
 	button: {
 		backgroundColor: "black",
 		borderRadius: 50,
-		paddingVertical: 15,
 		flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "center",
 		width: "100%",
 		position: "absolute",
 		bottom: 20,
+		height: 48,
 	},
 	text: {
 		color: "white",
