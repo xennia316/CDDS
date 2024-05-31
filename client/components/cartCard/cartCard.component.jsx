d7f3cb818c378187f81943b1880cdfce4c88a044
xennia316 <xennia316@gmail.com>
2024-05-31 05:02:24 +0100

    fix: make card disappear after closing

--- client/components/cartCard/cartCard.component.jsx
+++ client/components/cartCard/cartCard.component.jsx
@@ -4,7 +4,7 @@ import { View, Image, Text, TouchableOpacity } from "react-native";
 import styles from "./cartCard";
 
 const CartCard = (item) => {
-	const [number, setNumber] = useState(4);
+	const [number, setNumber] = useState(1);
 
 	const [showCard, setShowCard] = useState(true);
 
@@ -17,45 +17,53 @@ const CartCard = (item) => {
 	};
 
 	const handleClose = () => {
-		setShowCard(!showCard);
+		setShowCard(false);
+		console.log(showCard, "done");
 	};
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.rowImage}>
-				<Image
-					source={
-						item.image ? item.image : require("../../assets/images/orange.png")
-					}
-					style={styles.image}
-				/>
-			</View>
-			<View style={[styles.textRow]}>
-				<View>
-					<Text style={[styles.text, styles.largeText]}>
-						{item.title ? item.title : "Large Long Text Here"}
-					</Text>
-					{/* <Text style={[styles.text, styles.mediumText]}>Medium Text</Text> */}
-					<Text style={[styles.text, styles.smallText]}>
-						${item.price ? item.price : 18.99}
-					</Text>
+		showCard && (
+			<View style={styles.container}>
+				<View style={styles.rowImage}>
+					<Image
+						source={
+							item.image
+								? item.image
+								: require("../../assets/images/orange.png")
+						}
+						style={styles.image}
+					/>
 				</View>
-				<View style={styles.controls}>
-					<TouchableOpacity style={styles.button} onPress={handleDecrease}>
-						<Text style={styles.buttonText}>-</Text>
-					</TouchableOpacity>
-					<Text style={[styles.text, styles.number]}>{number}</Text>
-					<TouchableOpacity style={styles.button} onPress={handleIncrease}>
-						<Text style={styles.buttonText}>+</Text>
+				<View style={[styles.textRow]}>
+					<View>
+						<Text style={[styles.text, styles.largeText]}>
+							{item.title ? item.title : "Large Long Text Here"}
+						</Text>
+						{/* <Text style={[styles.text, styles.mediumText]}>Medium Text</Text> */}
+						<Text style={[styles.text, styles.smallText]}>
+							${item.price ? item.price : 18.99}
+						</Text>
+					</View>
+					<View style={styles.controls}>
+						<TouchableOpacity style={styles.button} onPress={handleDecrease}>
+							<Text style={styles.buttonText}>-</Text>
+						</TouchableOpacity>
+						<Text style={[styles.text, styles.number]}>{number}</Text>
+						<TouchableOpacity style={styles.button} onPress={handleIncrease}>
+							<Text style={styles.buttonText}>+</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+				<View style={[styles.row, styles.closeRow]}>
+					<TouchableOpacity
+						style={styles.closeButton}
+						onPress={() => handleClose()}
+					>
+						<Text style={styles.closeButtonText}>x</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
-			<View style={[styles.row, styles.closeRow]}>
-				<TouchableOpacity style={styles.closeButton} onPress={handleClose}>
-					<Text style={styles.closeButtonText}>x</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
+		)
 	);
 };
 
